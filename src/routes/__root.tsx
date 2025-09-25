51275dfde5a2239ce2eb1f0ed6c67d7189339085
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2025-09-24 21:42:02 -0300

    fix: ajustar exibição do header

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,10 @@
 import { Button, Group, Text, Flex, Divider } from '@mantine/core'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useMatchRoute,
+} from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import classes from './__root.module.css'
 
@@ -8,71 +13,93 @@ export const Route = createRootRoute({
 })
 
 function RootComponent() {
+  const matchRoute = useMatchRoute()
+  const paginaInicial =
+    matchRoute({ from: '/', to: '/' }) ||
+    matchRoute({ from: '/', to: '/login' }) ||
+    matchRoute({ from: '/', to: '/cadastro' }) ||
+    matchRoute({ from: '/', to: '/psicologo/$id' }) ||
+    matchRoute({ from: '/', to: '/about' })
+
   return (
     <>
-      <header style={{ height: 45 }}>
-        <Flex justify="space-between" align="center" h="100%" w="100%" px="md">
-          {/* esquerda */}
-          <Flex flex={1} justify="flex-start">
-            <Text size="xl" fw={700} c="blue" component={Link} to="/">
-              MindMeet
-            </Text>
-          </Flex>
+      {paginaInicial && (
+        <header style={{ height: 45 }}>
+          <Flex
+            justify="space-between"
+            align="center"
+            h="100%"
+            w="100%"
+            px="md"
+          >
+            {/* esquerda */}
+            <Flex flex={1} justify="flex-start">
+              <Text size="xl" fw={700} c="blue" component={Link} to="/">
+                MindMeet
+              </Text>
+            </Flex>
 
-          {/* meio */}
-          <Flex flex={1} justify="center">
-            <Group gap="lg">
-              <Text
-                component={Link}
-                className={classes.link}
-                to="/about"
-                activeOptions={{ exact: true }}
-                activeProps={{
-                  className: `${classes.link} ${classes.active}`,
-                }}
-              >
-                Sobre
-              </Text>
-              <Text
-                component={Link}
-                className={classes.link}
-                to="/app"
-                activeOptions={{ exact: true }}
-                activeProps={{
-                  className: `${classes.link} ${classes.active}`,
-                }}
-              >
-                Dashboard
-              </Text>
-              <Text
-                component={Link}
-                className={classes.link}
-                to="/psicologo/$id"
-                params={{ id: '1' }}
-                activeOptions={{ exact: true }}
-                activeProps={{
-                  className: `${classes.link} ${classes.active}`,
-                }}
-              >
-                Perfil Público
-              </Text>
-            </Group>
-          </Flex>
+            {/* meio */}
+            <Flex flex={1} justify="center">
+              <Group gap="lg">
+                <Text
+                  component={Link}
+                  className={classes.link}
+                  to="/about"
+                  activeOptions={{ exact: true }}
+                  activeProps={{
+                    className: `${classes.link} ${classes.active}`,
+                  }}
+                >
+                  Sobre
+                </Text>
+                <Text
+                  component={Link}
+                  className={classes.link}
+                  to="/app"
+                  activeOptions={{ exact: true }}
+                  activeProps={{
+                    className: `${classes.link} ${classes.active}`,
+                  }}
+                >
+                  Dashboard
+                </Text>
+                <Text
+                  component={Link}
+                  className={classes.link}
+                  to="/psicologo/$id"
+                  params={{ id: '1' }}
+                  activeOptions={{ exact: true }}
+                  activeProps={{
+                    className: `${classes.link} ${classes.active}`,
+                  }}
+                >
+                  Perfil Público
+                </Text>
+              </Group>
+            </Flex>
 
-          {/* direita */}
-          <Flex flex={1} justify="flex-end">
-            <Group>
-              <Button variant="outline" size="xs" component={Link} to="/login">
-                Entrar
-              </Button>
-              <Button size="xs" component={Link} to="/cadastro">
-                Cadastrar-se
-              </Button>
-            </Group>
+            {/* direita */}
+            <Flex flex={1} justify="flex-end">
+              <Group>
+                <Button
+                  variant="outline"
+                  size="xs"
+                  component={Link}
+                  to="/login"
+                >
+                  Entrar
+                </Button>
+                <Button size="xs" component={Link} to="/cadastro">
+                  Cadastrar-se
+                </Button>
+              </Group>
+            </Flex>
           </Flex>
-        </Flex>
-        <Divider />
-      </header>
+          <Divider />
+        </header>
+      )}
+
       <main>
         <Outlet />
       </main>
