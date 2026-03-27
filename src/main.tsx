df3da2bfb6ec91d3715960b4a24f3d3f4580e95a
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-03-27 19:30:30 -0300

    chore: ajustar configuração de datas

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css'
-import '@mantine/dates/styles.css';
+import '@mantine/dates/styles.css'
 
 import { MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
+
+dayjs.locale('pt-br')
 
 const router = createRouter({ routeTree })
 
