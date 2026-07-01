fc69df46f74512bb448edb9eef372c12e96ed2f3
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-07-01 18:14:48 -0300

    chore: ajustar configurações para variáveis de ambiente

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  build: {
+    target: 'es2022',
+  },
   plugins: [
     tanstackRouter({
       target: 'react',
