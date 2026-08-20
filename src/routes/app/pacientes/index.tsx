500c938320709ef6a04921e9b50dac2d2344fe99
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-08-20 18:53:31 -0300

    feat: alterar campos de tabela de listagem de pacientes

--- src/routes/app/pacientes/index.tsx
+++ src/routes/app/pacientes/index.tsx
@@ -9,15 +9,8 @@ import {
   Flex,
   Badge,
 } from '@mantine/core'
-import {
-  IconPlus,
-  IconEye,
-  IconEdit,
-  IconPhone,
-  IconMail,
-} from '@tabler/icons-react'
+import { IconPlus, IconEye, IconEdit } from '@tabler/icons-react'
 import { useState } from 'react'
-import dayjs from 'dayjs'
 import { PageLayout } from '../../../components/layout'
 import { usePacienteList } from '../../../api/endpoints/pacientes/pacientes'
 import { useAlterarTitle } from '../../../hooks/useAlterarTitle'
@@ -34,9 +27,8 @@ export const Route = createFileRoute('/app/pacientes/')({
 
 const COLUNAS_PACIENTES = [
   { chave: 'paciente', label: 'Paciente' },
-  { chave: 'contato', label: 'Contato' },
-  { chave: 'idade', label: 'Idade' },
-  { chave: 'queixa', label: 'Queixa Principal' },
+  { chave: 'telefone', label: 'Telefone' },
+  { chave: 'email', label: 'E-mail' },
   { chave: 'acoes', label: 'Ações', largura: 100 },
 ]
 
@@ -85,26 +77,10 @@ function TabelaPacientes() {
         </Group>
       </Table.Td>
       <Table.Td>
-        <div>
-          <Group gap="xs">
-            <IconPhone style={{ width: rem(12), height: rem(12) }} />
-            <Text size="xs">{paciente.numero_telefone}</Text>
-          </Group>
-          <Group gap="xs">
-            <IconMail style={{ width: rem(12), height: rem(12) }} />
-            <Text size="xs">{paciente.email}</Text>
-          </Group>
-        </div>
+        <Text size="xs">{paciente.numero_telefone}</Text>
       </Table.Td>
       <Table.Td>
-        <Text size="sm">
-          {dayjs().diff(dayjs(paciente.data_nascimento), 'year')} anos
-        </Text>
-      </Table.Td>
-      <Table.Td>
-        <Text lineClamp={2}>
-          {paciente.informacoes_clinicas?.queixa_principal ?? 'Nenhuma'}
-        </Text>
+        <Text size="xs">{paciente.email}</Text>
       </Table.Td>
       <Table.Td>
         <Flex gap="xs">
