51e7fe0e1e499d056e9a6ef37aff1776a8bd7e9f
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-09-02 10:14:48 -0300

    feat: alterar configurações padrões do cliente de query

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,44 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 
-import { routeTree } from "./routeTree.gen";
-import "./index.css";
-import "@mantine/core/styles.css";
-import "@mantine/notifications/styles.css";
-import "@mantine/dates/styles.css";
+import { routeTree } from './routeTree.gen'
+import './index.css'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import '@mantine/dates/styles.css'
 
-import { MantineProvider } from "@mantine/core";
-import { Notifications } from "@mantine/notifications";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "dayjs/locale/pt-br";
-import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import { theme } from "./theme";
-dayjs.extend(customParseFormat);
-dayjs.locale("pt-br");
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import { theme } from './theme'
 
-const router = createRouter({ routeTree });
+dayjs.extend(customParseFormat)
+dayjs.locale('pt-br')
 
-declare module "@tanstack/react-router" {
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
 
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = ReactDOM.createRoot(rootElement)
 
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: false,
+      },
+    },
+  })
 
   root.render(
     <StrictMode>
@@ -40,6 +48,6 @@ if (!rootElement.innerHTML) {
           <RouterProvider router={router} />
         </MantineProvider>
       </QueryClientProvider>
-    </StrictMode>,
-  );
+    </StrictMode>
+  )
 }
